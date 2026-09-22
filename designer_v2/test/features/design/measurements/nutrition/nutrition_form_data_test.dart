6b7a4be653a06f817847baacd1b70c6ed8456c90
Johannes Vedder <hallo@johannesvedder.de>
2026-09-22 11:23:07 +0200

    fix(designer): preserve nutrition schedule rules

--- designer_v2/test/features/design/measurements/nutrition/nutrition_form_data_test.dart
+++ designer_v2/test/features/design/measurements/nutrition/nutrition_form_data_test.dart
@@ -3,20 +3,53 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:studyu_core/core.dart';
 import 'package:studyu_designer_v2/features/design/measurements/nutrition/nutrition_form_controller.dart';
 import 'package:studyu_designer_v2/features/design/measurements/nutrition/nutrition_form_data.dart';
+import 'package:studyu_designer_v2/localization/app_localizations_en.dart';
+import 'package:studyu_designer_v2/localization/app_translation.dart';
 
 void main() {
-  test('designer edit round-trips the nutrition task schedule rule', () {
+  setUpAll(() => AppTranslation.setForTesting(AppLocalizationsEn()));
+
+  test(
+    'designer edit save round-trips the nutrition task schedule rule',
+    () async {
+      final task = NutritionTask.withId()
+        ..title = 'Nutrition'
+        ..scheduleRule = TaskScheduleRule.forEveryNDays(3, startOffset: 1);
+      final viewModel = NutritionFormViewModel(
+        study: Study('study', 'owner'),
+        formData: NutritionFormData.fromDomainModel(task),
+      );
+      viewModel.titleControl.value = 'Updated nutrition';
+
+      await viewModel.save();
+
+      final savedTask = (viewModel.formData!).toNutritionTask();
+      expect(savedTask.scheduleRule?.toJson(), task.scheduleRule?.toJson());
+    },
+  );
+
+  test('duplicating nutrition form data preserves the schedule rule', () {
     final task = NutritionTask.withId()
       ..title = 'Nutrition'
-      ..scheduleRule = TaskScheduleRule.forEveryNDays(3, startOffset: 1);
-    final formData = NutritionFormData.fromDomainModel(task);
+      ..scheduleRule = TaskScheduleRule.forSpecificDays([2, 5]);
     final viewModel = NutritionFormViewModel(
       study: Study('study', 'owner'),
-      formData: formData,
+      formData: NutritionFormData.fromDomainModel(task),
     );
 
-    final savedTask = viewModel.buildFormData().toNutritionTask();
+    final duplicateTask = viewModel
+        .createDuplicate()
+        .buildFormData()
+        .toNutritionTask();
 
-    expect(savedTask.scheduleRule?.toJson(), task.scheduleRule?.toJson());
+    expect(duplicateTask.scheduleRule?.toJson(), task.scheduleRule?.toJson());
+  });
+
+  test('new nutrition tasks have no schedule rule', () {
+    final viewModel = NutritionFormViewModel(study: Study('study', 'owner'));
+
+    final newTask = viewModel.buildFormData().toNutritionTask();
+
+    expect(newTask.scheduleRule, isNull);
   });
 }
